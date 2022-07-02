82249e2139c219bc8a2ed100b9a971dff165fa32
sonalibirla28 <sonalibilrla2802@gmail.com>
2022-07-02 16:20:33 +0530

    day 5 commit

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,54 +7,57 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset("assets/images/login_images.png",
-          fit: BoxFit.cover,),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-              "welcome",
-               style: TextStyle(
-                 fontSize: 24,
-                 fontWeight: FontWeight.bold,
-               ),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
-            child: Column(
-              children: [
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: "Enter username",
-                labelText: "Username",
-              ),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_images.png",
+            fit: BoxFit.cover,
             ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                hintText: "Enter password",
-                labelText: "Password",
-              ),
+            SizedBox(
+              height: 20.0,
             ),
-                SizedBox(
-                  height: 20.0,
+            Text(
+                "welcome",
+                 style: TextStyle(
+                   fontSize: 28,
+                   fontWeight: FontWeight.bold,
+                 ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+              child: Column(
+                children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: "Enter username",
+                  labelText: "Username",
                 ),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter password",
+                  labelText: "Password",
+                ),
+              ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
 
-                ElevatedButton(
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                    onPressed:(){},
-                )
+                  ElevatedButton(
+                      child: Text("Login"),
+                      style: TextButton.styleFrom(minimumSize: Size(100, 40)),
+                      onPressed:(){},
+                  )
     ],
-            ),
-          )
+              ),
+            )
 
-        ],
+          ],
+        ),
       )
     );
       /*child: Center(
